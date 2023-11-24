b0580de3a6c3dfd8c539550aa3c67fac01eab5b6
yeonsookang <yeonsoo.kang@lapiscine.pro>
2023-11-24 11:17:29 +0100

    fonction anonyme

--- src/pages/CocktailListPage.jsx
+++ src/pages/CocktailListPage.jsx
@@ -5,12 +5,36 @@ function CocktailListPage (){
     const [cocktailList, setCocktailList] = useState(null)    
 
     if(!cocktailList){
+
+        // vieux syntaxe
+        /*
         fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=")
         .then((response)=>{
             return response.json()
         }).then((json)=>{
             setCocktailList(json.drinks)
         })
+        */
+
+
+        // nouvelle syntaxe
+        /*
+        const fetchCocktails = async() =>{
+            const cocktailsResponse = await fetch()    // 이때, await는 async가 필요한데, async를 composant만드는 함수에 쓸 수 없으므로 안에 함수를 하나 더 만든다.
+            const cocktailsInJs = cocktailsResponse.json()
+        }
+        fetchCocktails();
+        */
+               // vu que c'est une fonction asynchrone, ca doit etre bizarre de l'appeller sans await.
+
+
+        // fonction anonyme (elle a pas de nom) qui s'autoinvoque
+        // cette syntaxe est comme les syntaxes au-dessus, mais cette fonction apelle elle-meme
+        (async () => {
+            const cocktailsResponse = await fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=")    // 이때, await는 async가 필요한데, async를 composant만드는 함수에 쓸 수 없으므로 안에 함수를 하나 더 만든다.
+            const cocktailsInJs = await cocktailsResponse.json()
+            setCocktailList(cocktailsInJs.drinks)
+        })(); // elle s'appelle elle meme . premier parentaise veut dire  fonction anonyme
     }
     console.log(cocktailList);
 
