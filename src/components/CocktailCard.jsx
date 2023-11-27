ceb21cd76945323750ee5c323a3299d675af0e43
yeonsookang <yeonsoo.kang@lapiscine.pro>
2023-11-27 16:16:58 +0100

    add fetch for delete

--- src/components/CocktailCard.jsx
+++ src/components/CocktailCard.jsx
@@ -1,6 +1,14 @@
 import { Link } from "react-router-dom"
 
 function CocktailCard({cocktail, keys}){
+
+    const removeCocktail = async(event, id) =>{
+        await fetch ("https.//www.delete/api=" +id , {method:"DELETE"})
+        // pour ma fonction qui va appeler au moment de clique, on va faire fetch pour supprimer.
+        // il faut ecrire methode HTTP: soit 'delete'
+        // pour le deuxieme parametre , on va recupere cocktail.idDrink
+    }
+
     return(
         <article>
             <p>{cocktail.strDrink}</p>
@@ -16,6 +24,9 @@ function CocktailCard({cocktail, keys}){
                 })}
             </ul>
             <Link to={`/cocktails/details/${cocktail.idDrink}`}>voir plus</Link>
+            <button onClick={(event)=>{removeCocktail(event,cocktail.idDrink)}}>supprimer le cocktail</button>
+            {/* quand je clique, j'appelle ma fonction 'removeCocktail' avec deux parametres. mais pour utiliser parametre sur onClick, */}
+            {/* il faut appeler une autre fonctioin pour appeler ma fonction. ()=>{} */}
         </article>
     )
 }
